246005f8fa626d8ca4dfb551927fb16c986ce3a2
Mykhailo Yeremeiev <mikle33435@gmail.com>
2024-03-10 19:07:03 +0200

    Add test plagin for Wordpress

---
+++ test-plugin-for-wp.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @package test-plugin-for-wp
+ * @version 1
+ */
+/*
+Plugin Name: test-plugin-for-wp
+Text Domain: test-plugin-for-wp
+Plugin URI: https://wordpress.org/plugins/test-plugin-for-wp/
+Description: Тестовый плагін по завданню 1.
+Author: Mykhailo
+Author URI: https://#
+Version: 1
+License: GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+function my_function() {
+    print("Hello World");
+ }
